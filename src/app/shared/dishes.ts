0760ef750be7e5c31c7796b876347cbaae7c4cce
Touis Salah <salahtouispro@gmail.com>
2022-08-08 14:37:19 +0100

    fourth

---
+++ src/app/shared/dishes.ts
@@ -0,0 +1,173 @@
+import { Dish } from "./dish";
+
+export const DISHES: Dish[]= [
+    {
+      id: '0',
+      name: 'couscous tunisien',
+      image: '/assets/images/couscous.jpg',
+      category: 'plat principal',
+      featured: false,
+      label: '',
+      price: '13',
+      description: "La légende dit qu'aujourd'hui tous les tunisiens préparent le couscous 😋😋😋",
+      comments: [
+        {
+            rating: 5,
+            comment: 'Imagine all the eatables, living in conFusion!',
+            author: 'John Lemon',
+            date: '2012-10-16T17:57:28.556094Z'
+        },
+        {
+            rating: 4,
+            comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+            author: 'Paul McVites',
+            date: '2014-09-05T17:57:28.556094Z'
+        },
+        {
+            rating: 3,
+            comment: 'Eat it, just eat it!',
+            author: 'Michael Jaikishan',
+            date: '2015-02-13T17:57:28.556094Z'
+        },
+        {
+            rating: 4,
+            comment: 'Ultimate, Reaching for the stars!',
+            author: 'Ringo Starry',
+            date: '2013-12-02T17:57:28.556094Z'
+        },
+        {
+            rating: 2,
+            comment: 'It\'s your birthday, we\'re gonna party!',
+            author: '25 Cent',
+            date: '2011-12-02T17:57:28.556094Z'
+        }
+    ]
+    },
+    {
+      id: '1',
+      name: 'Macarouna Bel Salsa - Ô petits délices',
+      image: '/assets/images/Macarouna Bel Salsa.jpg',
+      category: 'plat principal',
+      featured: false,
+      label: '',
+      price: '12',
+      description: "Macarouna bel salsa au bœuf pâtes à la tunisienne 1kg de bœuf bourguignon ou 1 poulet 4/5 gousses d'ail pilés 1 cas de felfel maison 2 à 3 c à soupe de conçentré de tomate 1 tasse à café d'huile d'olives 1 cac de piment rouge en poudre fort 1 cac de...",
+      comments: [
+        {
+            rating: 5,
+            comment: 'Imagine all the eatables, living in conFusion!',
+            author: 'John Lemon',
+            date: '2012-10-16T17:57:28.556094Z'
+        },
+        {
+            rating: 4,
+            comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+            author: 'Paul McVites',
+            date: '2014-09-05T17:57:28.556094Z'
+        },
+        {
+            rating: 3,
+            comment: 'Eat it, just eat it!',
+            author: 'Michael Jaikishan',
+            date: '2015-02-13T17:57:28.556094Z'
+        },
+        {
+            rating: 4,
+            comment: 'Ultimate, Reaching for the stars!',
+            author: 'Ringo Starry',
+            date: '2013-12-02T17:57:28.556094Z'
+        },
+        {
+            rating: 2,
+            comment: 'It\'s your birthday, we\'re gonna party!',
+            author: '25 Cent',
+            date: '2011-12-02T17:57:28.556094Z'
+        }
+    ]
+    },
+    {
+      id: '2',
+      name: 'le riz au poisson',
+      image: '/assets/images/le riz au poisson.jpg',
+      category: 'plat principal',
+      featured: false,
+      label: 'New',
+      price: '13',
+      description: "Vous connaissez déjà le fameux tieboudieune (le riz au poisson) mais également le yassa poulet, qui a un succès fou sur le blog. Lorsqu'on parle de tieb (thieb), on parle de riz, il peut ainsi avoir différentes variantes, comme avec cette recette au poulet....",
+      comments: [
+        {
+            rating: 5,
+            comment: 'Imagine all the eatables, living in conFusion!',
+            author: 'John Lemon',
+            date: '2012-10-16T17:57:28.556094Z'
+        },
+        {
+            rating: 4,
+            comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+            author: 'Paul McVites',
+            date: '2014-09-05T17:57:28.556094Z'
+        },
+        {
+            rating: 3,
+            comment: 'Eat it, just eat it!',
+            author: 'Michael Jaikishan',
+            date: '2015-02-13T17:57:28.556094Z'
+        },
+        {
+            rating: 4,
+            comment: 'Ultimate, Reaching for the stars!',
+            author: 'Ringo Starry',
+            date: '2013-12-02T17:57:28.556094Z'
+        },
+        {
+            rating: 2,
+            comment: 'It\'s your birthday, we\'re gonna party!',
+            author: '25 Cent',
+            date: '2011-12-02T17:57:28.556094Z'
+        }
+    ]
+    },
+    {
+      id: '3',
+      name: 'Gastronomie tunisienne',
+      image: '/assets/images/Gastronomie tunisienne.jpg',
+      category: 'mains',
+      featured: true,
+      label: 'Dessert',
+      price: '4.9',
+      description: " Ils utilisent généralement la matière première de l'environnement, à savoir le miel, les noix (amandes, noix, pistaches et pignons) et les dattes… 🇬🇧 Ils utilisent généralement la matière première de l'environnement…" ,
+      comments: [
+        {
+            rating: 5,
+            comment: 'Imagine all the eatables, living in conFusion!',
+            author: 'John Lemon',
+            date: '2012-10-16T17:57:28.556094Z'
+        },
+        {
+            rating: 4,
+            comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+            author: 'Paul McVites',
+            date: '2014-09-05T17:57:28.556094Z'
+        },
+        {
+            rating: 3,
+            comment: 'Eat it, just eat it!',
+            author: 'Michael Jaikishan',
+            date: '2015-02-13T17:57:28.556094Z'
+        },
+        {
+            rating: 4,
+            comment: 'Ultimate, Reaching for the stars!',
+            author: 'Ringo Starry',
+            date: '2013-12-02T17:57:28.556094Z'
+        },
+        {
+            rating: 2,
+            comment: 'It\'s your birthday, we\'re gonna party!',
+            author: '25 Cent',
+            date: '2011-12-02T17:57:28.556094Z'
+        }
+    ]
+    }
+   ];
+  
